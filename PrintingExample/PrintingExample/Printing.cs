6e63a9799fc3328b5adcebec7f50f822a11b814e
Tiffany Ford <rhysma@gmail.com>
2017-10-05 13:58:23 -0500

    Adding the printing example project

---
+++ PrintingExample/PrintingExample/Printing.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Drawing;
+using System.Drawing.Printing;
+
+namespace PrintingExample
+{
+    class Printing : PrintDocument
+    {
+        private Font font;
+
+        public Font PrinterFont
+        {
+            //property to hold the font the user wants to use
+            get { return font; }
+            set { font = value; }
+        }
+        private string text;
+
+        public string TextToPrint
+        {
+            //property that holds the text to be printed
+            get { return text; }
+            set { text = value; }
+        }
+
+        protected override void OnBeginPrint(PrintEventArgs e)
+        {
+            //run base code
+            base.OnBeginPrint(e);
+
+            //check to see if the user provided a font
+            //if not, use a default
+            if (font == null)
+            {
+                font = new Font("Times New Roman", 12);
+            }
+        }
+
+        protected override void OnPrintPage(PrintPageEventArgs e)
+        {
+            //run base code
+            base.OnPrintPage(e);
+
+            //declare variables
+            int printHeight;
+            int printWidth;
+            int leftMargin;
+            int rightMargin;
+
+            //set the print area and margins
+            printHeight = base.DefaultPageSettings.PaperSize.Height -
+                base.DefaultPageSettings.Margins.Top -
+                base.DefaultPageSettings.Margins.Bottom;
+
+            printWidth = base.DefaultPageSettings.PaperSize.Width -
+                base.DefaultPageSettings.Margins.Left -
+                base.DefaultPageSettings.Margins.Right;
+
+            leftMargin = base.DefaultPageSettings.Margins.Left;
+            rightMargin = base.DefaultPageSettings.Margins.Right;
+
+        }
+    }
+}
