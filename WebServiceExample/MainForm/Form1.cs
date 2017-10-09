f4cac939a9da1baebd24913bb0f2ae2b676b05c7
Tiffany Ford <rhysma@gmail.com>
2017-10-09 14:48:18 -0500

    adding the web service example project

---
+++ WebServiceExample/MainForm/Form1.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using MainForm.ServiceReference1;
+
+namespace MainForm
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void SubmitButton_Click(object sender, EventArgs e)
+        {
+            //get the data from the combo box
+            string size = SizeBox.SelectedItem.ToString();
+
+            //setup an instance to the web service
+            ShirtServiceClient myClient = new ShirtServiceClient();
+
+            //access the method we want to use on the service
+            decimal price = myClient.ShirtPrices(size);
+
+            //display the decimal result
+            ResponseLabel.Text = string.Format("Price: {0:C}", price);
+        }
+    }
+}
---
+++ WebServiceExample/ShirtService/Service1.svc.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.ServiceModel.Web;
+using System.Text;
+
+namespace ShirtService
+{
+    // NOTE: You can use the "Rename" command on the "Refactor" menu to change the class name "Service1" in code, svc and config file together.
+    // NOTE: In order to launch WCF Test Client for testing this service, please select Service1.svc or Service1.svc.cs at the Solution Explorer and start debugging.
+    public class MyShirtService : IShirtService
+    {
+        public string GetData(int value)
+        {
+            return string.Format("You entered: {0}", value);
+        }
+
+        public decimal ShirtPrices(string size)
+        {
+            decimal price = 0.0m; 
+
+            switch(size)
+            {
+                case "Small":
+                    price = 10.00m;
+                    break;
+
+                case "Medium":
+                    price = 15.00m;
+                    break;
+
+                case "Large":
+                    price = 20.00m;
+                    break;
+            }
+
+            return price;
+        }
+
+    }
+}
