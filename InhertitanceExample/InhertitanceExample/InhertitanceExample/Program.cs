3d2d89b86a645f29bbd8cc0fa975571a7d2761bd
FORD <rhysma@gmail.com>
2022-08-30 09:20:06 -0500

    adding inheritance example from textbook

---
+++ InhertitanceExample/InhertitanceExample/InhertitanceExample/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace InhertitanceExample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("***** Basic Inheritance *****\n");
+
+            // Make a Car object and set max speed.
+            Car myCar = new Car(80);
+
+            // Set the current speed, and print it.
+            myCar.Speed = 50;
+
+            Console.WriteLine("My car is going {0} MPH", myCar.Speed);
+
+            // Now make a MiniVan object.
+            MiniVan myVan = new MiniVan();
+
+            myVan.Speed = 10;
+
+            Console.WriteLine("My van is going {0} MPH", myVan.Speed);
+
+            Console.ReadLine();
+
+        }
+    }
+}
